15477cb4a7387c280d7c62dadf9bc85a8159bc76
Alpha1294 <radugabriel1294@gmail.com>
2023-10-24 19:11:28 +0200

    another minor mistake solved

--- sql_lab_queries.sql
+++ sql_lab_queries.sql
@@ -34,10 +34,11 @@ SELECT title, length FROM film ORDER BY length DESC limit 10;
 SELECT * FROM actor WHERE first_name = "SCARLETT";
 -- BONUS:
 -- 7.2 Retrieve all movies that have ARMAGEDDON in their title and have a duration longer than 100 minutes.
-SELECT * FROM film WHERE title LIKE "%ARMAGEDON&" AND length > 100;
+SELECT * FROM film WHERE title LIKE '%ARMAGEDDON%' AND length>100;
 -- Hint: use LIKE operator. More information here.
 -- 7.3 Determine the number of films that include Behind the Scenes content
 SELECT COUNT(*) FROM film WHERE special_features LIKE "%Behind the scenes%";
 
-SELECT * FROM film;
+
+
 
